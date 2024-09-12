9e58e9a2547697c70d533582fd7484ae63900f03
voe <99111186+voeej@users.noreply.github.com>
2024-09-12 22:20:13 +0700

    Add files via upload

---
+++ Script/about.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class about : MonoBehaviour
+{
+ public void back() {
+     SceneManager.LoadScene("mainMenu");
+ }
+}
